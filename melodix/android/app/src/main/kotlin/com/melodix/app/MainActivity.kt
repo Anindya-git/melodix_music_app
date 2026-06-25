e634db7ec6615e5f19e808f44eb03cbd58d04bc7
Anindya Pandob Rahul <anindya.pandob.rahul@g.bracu.ac.bd>
2026-06-25 10:29:04 +0600

    Delete melodix directory

--- melodix/android/app/src/main/kotlin/com/melodix/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.melodix.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- melodix/lib/models/song_model.dart
+++
@@ -1,2 +0,0 @@
-// Re-export the clean model so imports of song_model.dart still work
-export 'song_model_clean.dart';
